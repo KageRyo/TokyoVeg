2c001ad7f4edfd66e6f723d2a7ca1563bc2ddcc8
影凌 <k6.developer@gmail.com>
2024-07-09 10:51:43 +0800

    feat: Add models.

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    name = models.CharField(max_length=20, help_text='輸入商店名稱')
+    address = models.CharField(max_length=50, null=True)
+    pub_date = models.DateTimeField('上傳日期')
+
+    def __str__(self):
+        return self.name
