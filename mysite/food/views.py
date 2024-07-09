fe6336674474a6a68b8fd8da2933038f146f0abf
影凌 <k6.developer@gmail.com>
2024-07-09 15:16:23 +0800

    feat: modify views.py & index.html to load store

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import Place
 
 # Create your views here.
 def index(request):
-    return render(request, 'food/index.html', {'place_nums': range(6)})
+    return render(request, 'food/index.html', {'store_list': Place.objects.all(), })
