d33c96696c595b2d27a620d6ccc790bb77a59113
影凌 <k6.developer@gmail.com>
2024-07-12 16:32:30 +0800

    feat: Update admin.py

--- mysite/food/admin.py
+++ mysite/food/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Place, Photo, Tag, Tag_Management, Device, Device_Management
 
-admin.site.register(Place, admin.ModelAdmin)                # 商店資訊
+class TagInlineAdmin(admin.StackedInline):
+    model = Place.tags.through
+    
+class DeviceInlineAdmin(admin.StackedInline):
+    model = Place.devices.through
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'address', 'website', 'pub_date')
+    list_filter = ('pub_date', 'tags')
+    search_fields = ('name', 'address', 'website')
+    ordering = ('-id',)
+    readonly_fields = ('pub_date',)
+    inlines = [TagInlineAdmin, DeviceInlineAdmin]
+
+admin.site.register(Place, PlaceAdmin)                      # 商店資訊
 admin.site.register(Photo, admin.ModelAdmin)                # 商店照片
 admin.site.register(Tag, admin.ModelAdmin)                  # 標籤
 admin.site.register(Tag_Management, admin.ModelAdmin)       # 標籤與店家關聯
