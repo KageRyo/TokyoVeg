451f07af20b541f34071b56411d9b24b69aedad4
影凌 <k6.developer@gmail.com>
2024-07-16 14:39:41 +0800

    feat: Add Sentry

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -136,3 +136,22 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+SENTRY_CLIENT_DSN = os.getenv('SENTRY_CLIENT_DSN')
+sentry_sdk.init(
+    dsn=SENTRY_CLIENT_DSN,
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=0.01,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
